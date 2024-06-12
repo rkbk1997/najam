d571bc215e875bc37309b2be6b530341efcd0784
Shahanas Abdul Jaleel <shahnaz.jaleel@machinestalk.com>
2024-06-12 13:35:48 +0300

    Employee list update
    Employee list update

--- src/app/modules/dashboard/component/employee-details/employee-details.component.ts
+++ src/app/modules/dashboard/component/employee-details/employee-details.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { DashboardService } from '../../../../services/dashboard/dashboard.service';
+import { LangService } from '../../../../services/lang/lang.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-employee-details',
@@ -6,5 +9,47 @@ import { Component } from '@angular/core';
   styleUrl: './employee-details.component.scss'
 })
 export class EmployeeDetailsComponent {
+  appLang!: string;
+  newsDetails: any = {};
 
+  /**
+   * constructor
+   * @param dashboardService 
+   * @param LanguageService 
+   */
+  constructor(private dashboardService: DashboardService, private LanguageService: LangService, private activateRoute: ActivatedRoute) { }
+
+
+  /**
+   * ngOnInit Life cycle hook
+   */
+  ngOnInit(): void {
+    this.LanguageService.languageSubject$.subscribe((res) => {
+      this.appLang = res;
+    });
+
+    this.activateRoute.queryParams.subscribe((queryParams:any) => {
+      if(queryParams.id) {
+        this.getEmployeeDetails(queryParams.id);
+      }
+    });
+  }
+
+
+  getEmployeeDetails(id: any) {
+    this.dashboardService.getRecoardById(id, this.appLang).subscribe((res:any) => {
+      this.newsDetails = res;
+      
+    })
+  }
+
+
+    /**
+   * get image url 
+   * @param url 
+   * @returns 
+   */
+    getImageUrl(url: string) {
+      return this.dashboardService.getImageUrl(url);
+    }
 }
